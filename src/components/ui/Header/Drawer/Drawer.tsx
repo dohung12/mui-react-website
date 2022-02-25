6bf56cacb795518155091a75ce6813209646913a
dohung <dohuyhung12@gmail.com>
2022-02-25 09:12:04 +0700

    add routes to drawer

--- src/components/ui/Header/Drawer/Drawer.tsx
+++ src/components/ui/Header/Drawer/Drawer.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
-import { SwipeableDrawer, IconButton } from '@mui/material'
+import {
+  SwipeableDrawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material'
 import { Menu } from '@mui/icons-material'
+import { Link, useLocation } from 'react-router-dom'
+import { getRoutesLink } from '../../../Routes'
 
 const Drawer = () => {
   const iOS =
     typeof navigator !== 'undefined' &&
     /iPad|iPhone|iPod/.test(navigator.userAgent)
   const [openDrawer, setOpenDrawer] = useState(false)
+  const location = useLocation().pathname
+  const routes = getRoutesLink()
 
   return (
     <>
@@ -17,8 +27,56 @@ const Drawer = () => {
         onClose={() => setOpenDrawer(false)}
         onOpen={() => setOpenDrawer(true)}
         anchor='left'
+        sx={{
+          '& .MuiDrawer-paper': {
+            backgroundColor: 'primary.main',
+            color: '#fff',
+          },
+        }}
       >
-        Example Drawer
+        <List disablePadding>
+          {routes.map((route) => {
+            const { name, link } = route
+            return (
+              <ListItem
+                key={name}
+                divider
+                button
+                component={Link}
+                to={link}
+                onClick={() => setOpenDrawer(false)}
+                sx={{
+                  typography: 'tab',
+                  color: '#fff',
+                  opacity: 0.7,
+
+                  '&.Mui-selected': {
+                    color: 'secondary.main',
+                    opacity: 1,
+                    backgroundColor: '#075985',
+                  },
+                }}
+                selected={location === link}
+              >
+                <ListItemText disableTypography>{name}</ListItemText>
+              </ListItem>
+            )
+          })}
+          <ListItem
+            sx={{
+              typography: 'tab',
+              color: '#fff',
+              backgroundColor: 'secondary.main',
+            }}
+            divider
+            button
+            component={Link}
+            to='/estimate'
+            onClick={() => setOpenDrawer(false)}
+          >
+            <ListItemText disableTypography>Free Estimate</ListItemText>
+          </ListItem>
+        </List>
       </SwipeableDrawer>
       <IconButton
         onClick={() => setOpenDrawer(!openDrawer)}
